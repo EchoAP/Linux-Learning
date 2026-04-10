a8d5948538b78a2428a1ca64298228807bb45e72
EchaAP <abstractterms@tuta.io>
2026-04-09 20:12:59 -0400

    sync updates

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,3 @@
 #This code prints the statement in paraentheses
-print('Hello, world! This is a my new Python file.')
+print('Hello, world! This is a my first Python file.')
+print('Annnd this line is me using nano to edit!!!')
